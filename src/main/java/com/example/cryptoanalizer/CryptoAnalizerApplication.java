5db30b2372fde9b578d41ba88b53ea6732f02475
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-08-04 17:28:51 +0200

    Project initialization

---
+++ src/main/java/com/example/cryptoanalizer/CryptoAnalizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptoanalizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoAnalizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoAnalizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cryptoanalizer/CryptoAnalizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptoanalizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoAnalizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
