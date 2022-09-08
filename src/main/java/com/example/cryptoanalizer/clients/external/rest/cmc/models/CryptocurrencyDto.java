604a46d35b5da688bb2cf89570918ffe41887eb9
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-09-08 16:40:47 +0200

    Change client dto's

--- src/main/java/com/example/cryptoanalizer/clients/external/rest/cmc/models/CryptocurrencyDto.java
+++ src/main/java/com/example/cryptoanalizer/clients/external/rest/cmc/models/CryptocurrencyDto.java
@@ -1,4 +1,4 @@
-package com.example.cryptoanalizer.clients.external.cmc.model;
+package com.example.cryptoanalizer.clients.external.rest.cmc.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
